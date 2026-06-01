08cbacdf7a0d1639743567ec81c39b1eff8b32f3
KaiseR0409 <fernandoignaciovalderramavega@gmail.com>
2026-06-01 15:09:57 -0400

    loading in excel button

--- frontend/src/components/UploadExcel.vue
+++ frontend/src/components/UploadExcel.vue
@@ -4,6 +4,7 @@ import { ref } from "vue"
 import api from "../api/analytics"
 import { toast } from "vue-sonner"
 
+const loading = ref(false)
 const file = ref(null)
 
 const emit = defineEmits([
@@ -21,6 +22,8 @@ const uploadFile = async () => {
 
   formData.append("file", file.value)
 
+  loading.value = true
+
   try {
 
     const response = await api.post(
@@ -28,7 +31,7 @@ const uploadFile = async () => {
       formData,
       {
         headers: {
-          "Content-Type":"multipart/form-data"
+          "Content-Type": "multipart/form-data"
         }
       }
     )
@@ -37,10 +40,12 @@ const uploadFile = async () => {
       response.data.clients
     )
     toast.success("Excel cargado correctamente")
-    
+
 
   } catch (error) {
     toast.error("Error al subir el archivo excel")
+  } finally {
+    loading.value = false
   }
 }
 
@@ -50,32 +55,38 @@ const uploadFile = async () => {
 
   <div class="flex gap-4 items-center">
 
-    <input
-      type="file"
-      @change="handleFileChange"
-      class="
+    <input type="file" @change="handleFileChange" class="
         bg-gray-800
         border
         border-gray-700
         rounded-xl
         px-4
         py-3
-      "
-    />
+      " />
 
-    <button
-      @click="uploadFile"
-      class="
+    <button @click="uploadFile" :disabled="loading" class="
         bg-purple-600
         hover:bg-purple-500
+        disabled:opacity-50
         transition
         px-6
         py-3
         rounded-xl
         font-semibold
-      "
-    >
-      Subir Excel
+      ">
+      <div v-if="loading" class="
+        w-5
+        h-5
+        border-2
+        border-white/30
+        border-t-white
+        rounded-full
+        animate-spin
+      "></div>
+
+      <span>
+        {{ loading ? "" : "Subir Excel" }}
+      </span>
     </button>
 
   </div>
